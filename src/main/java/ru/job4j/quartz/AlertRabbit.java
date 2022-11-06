4ef73c384a472715400528542bfa5c0fb73abe6d
Admin <Serger9393@mail.ru>
2022-11-07 02:23:52 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,18 +3,46 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import java.io.*;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Properties;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
-        try {
-            Properties properties = loadProperties("rabbit.properties");
+
+    private static Properties prop = new Properties();
+
+    private static Connection initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(prop.getProperty("driver_class"));
+        return DriverManager.getConnection(
+                prop.getProperty("url"),
+                prop.getProperty("login"),
+                prop.getProperty("password")
+        );
+    }
+
+    public static Properties loadProperties(String path) throws FileNotFoundException {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(path)) {
+            prop.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Properties properties = loadProperties("rabbit.properties");
+        try (Connection connection = initConnection()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(properties.getProperty("rabbit.interval")))
                     .repeatForever();
@@ -23,27 +51,35 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
-        } catch (FileNotFoundException e) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        } catch (InterruptedException | ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static Properties loadProperties(String path) throws FileNotFoundException {
-        Properties properties = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(path)) {
-            properties.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
-    }
-
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail()
+                    .getJobDataMap().get("connection");
+            try {
+                 PreparedStatement statement = cn.prepareStatement(
+                         "insert into rabbit(created_date) values (?)");
+                statement.setTimestamp(1,
+                        Timestamp.valueOf(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)));
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
