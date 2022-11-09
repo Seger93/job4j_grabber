d87ee5c5438c6da42eb1c284805f81d6e4b18c58
Admin <Serger9393@mail.ru>
2022-11-09 21:52:23 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,18 +15,21 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element titleDate = row.select(".vacancy-card__date").first();
-            Element time = titleDate.child(0);
-            String link = String.format("%s%s %n%s", SOURCE_LINK, linkElement.attr("href"),
-                    time.attr("datetime"));
-            System.out.printf("%s %s%n", vacancyName, link);
-        });
+        for (int i = 0; i <= 5; i++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                Element titleDate = row.select(".vacancy-card__date").first();
+                Element time = titleDate.child(0);
+                String link = String.format("%s%s %n%s", SOURCE_LINK, linkElement.attr("href"),
+                        time.attr("datetime"));
+                System.out.printf("%s %s%n", vacancyName, link);
+            });
+        }
     }
 }
+
