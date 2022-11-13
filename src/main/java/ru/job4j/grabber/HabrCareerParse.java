32e5af9a2cca02ab529430c46783018580e8f1e7
Admin <Serger9393@mail.ru>
2022-11-13 17:36:46 +0300

    2.3. Загрузка деталей поста. [#285212 #360345]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,19 +5,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
     private static final int PAGE = 5;
 
-   private String retrieveDescription(String link) throws IOException {
-       Connection connection = Jsoup.connect(link);
-       Document document = connection.get();
-           Elements description = document.select("style--ugc");
-           return  description.text();
-   }
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    private String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+            Element description = document.select(".style--ugc").first();
+            return description.text();
+    }
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= PAGE; i++) {
