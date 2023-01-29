e5e8ca75faef90aaf871b78213981663e5939361
Admin <Serger9393@mail.ru>
2023-01-29 18:15:15 +0300

    3. Что такое TDD? [#4918 #385962]

--- src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -1,16 +1,13 @@
 package ru.job4j.ood.tdd;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
-
 import java.util.Calendar;
 import java.util.List;
 
+@Disabled
 public class Cinema3DTest {
-    @Ignore
     @Test
     public void whenBuyThenGetTicket() {
         Account account = new AccountCinema();
@@ -20,7 +17,6 @@ public class Cinema3DTest {
         assertThat(ticket).isEqualTo(new Ticket3D());
     }
 
-    @Ignore
     @Test
     public void whenAddSessionThenItExistsBetweenAllSessions() {
         Cinema cinema = new Cinema3D();
@@ -30,7 +26,6 @@ public class Cinema3DTest {
         assertThat(sessions).contains(session);
     }
 
-    @Ignore
     @Test
     public void whenBuyOnInvalidRowThenGetException() {
         Account account = new AccountCinema();
@@ -40,7 +35,6 @@ public class Cinema3DTest {
                 isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
     @Test
     public void whenBuyOnInvalidColumnThenGetException() {
         Account account = new AccountCinema();
@@ -50,23 +44,21 @@ public class Cinema3DTest {
                 isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
     @Test
     public void whenInvalidDateException() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2022, 10, 12, 11, 00);
+        date.set(2022, Calendar.NOVEMBER, 12, 11, 0);
         assertThatThrownBy(() -> cinema.buy(account, 1, 1, date)).
                 isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
     @Test
     public void whenBuyTicketAndAccountDoesNotExist() {
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2022, 10, 12, 11, 00);
+        date.set(2022, Calendar.NOVEMBER, 12, 11, 0);
         assertThatThrownBy(() -> cinema.buy(null, 1, 1, date)).
                 isInstanceOf(IllegalArgumentException.class);
     }
